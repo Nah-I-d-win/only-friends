e5399505e22ee20972e0a6a2c7b4e78e81349976
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-02-09 00:22:25 +0100

    update colors

--- app/src/main/java/com/example/only_friends/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/only_friends/fragments/HomeFragment.kt
@@ -24,10 +24,11 @@ class HomeFragment(
         val view = inflater?.inflate(R.layout.fragment_home, container, false)
 
         val giftList = arrayListOf<GiftModel>()
-        giftList.add(GiftModel("Ducati Panigale V4", "26 490€  [o_O]", "https://images.ctfassets.net/x7j9qwvpvr5s/2zYQHBj5OxbGGvtcdiv2EK/a876ddf620f84c92a362522bcdc14a90/Panigale-MY22-Studio-42-Gallery-1920x1080.jpg", false))
+        giftList.add(GiftModel("Ducati Panigale V4", "26 490€  [o_O]", "https://images.ctfassets.net/x7j9qwvpvr5s/2zYQHBj5OxbGGvtcdiv2EK/a876ddf620f84c92a362522bcdc14a90/Panigale-MY22-Studio-42-Gallery-1920x1080.jpg", true))
         giftList.add(GiftModel("Yamaha R1", "20 499€  (ಥ﹏ಥ)", "https://cdn-sb.mozu.com/27878-44719/cms/files/016ffa83-d61f-4f5b-ada9-1d76b3a72329?_mzcb=_1680803352036&width=1009&quality=70", false))
         giftList.add(GiftModel("KTM 1390 SUPER DUKE R", "?? ???€  ¯\\_(ツ)_/¯", "https://www.cycleworld.com/resizer/vC5qBfzEZFKPLV7BtvnwYjab1fA=/arc-photo-octane/arc3-prod/public/SLIMO4K2J5ACDJMAU4HBSC3HUM.jpg", true))
-        giftList.add(GiftModel("Kawasaki Ninja H2R", "52 000€  ( ͡° ͜ʖ ͡°)", "https://cdn.pixabay.com/photo/2021/04/19/05/12/kawasaki-ninja-h2r-6190254_1280.jpg", true))
+        giftList.add(GiftModel("Kawasaki Ninja H2R", "52 000€  ( ͡° ͜ʖ ͡°)", "https://cdn.pixabay.com/photo/2021/04/19/05/12/kawasaki-ninja-h2r-6190254_1280.jpg", false))
+        giftList.add(GiftModel("One day", "They'll all be mine", "https://imgflip.com/s/meme/Smiling-Cat.jpg", true))
 
 
 
