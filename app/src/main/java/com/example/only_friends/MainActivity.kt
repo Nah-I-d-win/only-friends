3099580b68134e4793a9e713be3f115d1d11c0e1
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-02-09 22:53:23 +0100

    new nav bar

--- app/src/main/java/com/example/only_friends/MainActivity.kt
+++ app/src/main/java/com/example/only_friends/MainActivity.kt
@@ -2,24 +2,48 @@ package com.example.only_friends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import com.example.only_friends.fragments.AddGiftFragment
 import com.example.only_friends.fragments.CollectionFragment
 import com.example.only_friends.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFragment(HomeFragment(this), R.string.home_page_title)
+
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.home_page -> {
+                    loadFragment(HomeFragment(this), R.string.home_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }R.id.collection_page -> {
+                    loadFragment(CollectionFragment(this), R.string.collection_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }R.id.add_gift_page -> {
+                    loadFragment(AddGiftFragment(this), R.string.add_gift_page_title)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment, string: Int) {
         val repo = GiftRepository()
+        findViewById<TextView>(R.id.page_title).text = resources.getString(string)
         repo.updateData {
             // What to do once the data is loaded
             val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, HomeFragment(this))
+            transaction.replace(R.id.fragment_container, fragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
-
-
     }
 }
