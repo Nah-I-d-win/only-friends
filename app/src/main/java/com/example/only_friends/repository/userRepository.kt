816207797132ee56b858c75f36f82299af01928f
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-15 15:55:28 +0100

    add spash screen update theme for dark mode

---
+++ app/src/main/java/com/example/only_friends/repository/userRepository.kt
@@ -0,0 +1,4 @@
+package com.example.only_friends.repository
+
+class userRepository {
+}
---
+++ app/src/main/java/com/example/only_friends/view/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.only_friends.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.only_friends.R
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val imageView: ImageView = findViewById(R.id.image)
+        Glide.with(this).load(R.drawable.humain).into(imageView)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }, 3000) // 3 secondes de délai
+    }
+}
