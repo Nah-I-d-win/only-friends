9cd5480e689b3f12d5cbe9b9f08e13c0fe3b2c72
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-15 17:28:03 +0100

    add user model

--- app/src/main/java/com/example/only_friends/model/User.kt
+++ app/src/main/java/com/example/only_friends/model/User.kt
@@ -1,4 +1,13 @@
 package com.example.only_friends.model
 
-class User {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User (
+    @PrimaryKey val uid: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "email") val email: String
+
+)
