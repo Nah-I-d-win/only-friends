09d6f6570bfc3799d22e075d628d946c32d8f649
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-15 17:45:02 +0100

    build connexion activity

--- app/src/main/java/com/example/only_friends/view/LoginActivity.kt
+++ app/src/main/java/com/example/only_friends/view/LoginActivity.kt
@@ -1,12 +1,41 @@
 package com.example.only_friends.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.only_friends.MainActivity
 import com.example.only_friends.R
+import com.example.only_friends.viewModel.UserViewModel
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+    private lateinit var viewModel: UserViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val emailEditText = findViewById<EditText>(R.id.email)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+            viewModel.login(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // pour les messages
+                }
+
+            }
+        }
     }
 }
