46c9fedb121f30de09dbcd82f77c301f0252ab6b
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-15 15:58:38 +0100

    add sign up and connexion layou

---
+++ app/src/main/java/com/example/only_friends/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.only_friends.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.only_friends.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/only_friends/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.only_friends.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.only_friends.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
