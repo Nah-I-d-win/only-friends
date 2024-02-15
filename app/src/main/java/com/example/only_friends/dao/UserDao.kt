f103bb87c3e619aa5513292f989bdb00bb2cb4a8
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-15 17:25:02 +0100

    add user dao

--- app/src/main/java/com/example/only_friends/dao/UserDao.kt
+++ app/src/main/java/com/example/only_friends/dao/UserDao.kt
@@ -1,4 +1,20 @@
 package com.example.only_friends.dao
 
-class UserDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.only_friends.model.User
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user WHERE uid = :uid LIMIT 1")
+    fun getUser(uid: String): User?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User)
+
+    @Delete
+    fun delete(user: User)
 }
