3f2140732e7b99194476d4b53d6c4481a83d8172
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-02-17 18:13:42 +0100

    gestion des messages d'erreurs

--- app/src/main/java/com/example/only_friends/view/LoginActivity.kt
+++ app/src/main/java/com/example/only_friends/view/LoginActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -49,14 +50,29 @@ class LoginActivity : BaseActivity() {
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
+            val errorMessageTextView = findViewById<TextView>(R.id.error_message)
 
+
+            if (email.isEmpty() || password.isEmpty()) {
+                errorMessageTextView.text = "Veuillez remplir tous les champs."
+                return@setOnClickListener
+            }
             if (isConnectedToInternet()){
-                viewModel.login(email, password).addOnCompleteListener { task ->
+                viewModel.login(email, password)
+                    .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                     } else {
-                        // pour les messages
+                       //recuperer le message d'erreur
+                        val message = task.exception?.message
+                        AlertDialog.Builder(this)
+                            .setTitle("Erreur")
+                            .setMessage(message)
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                            .show()
                     }
 
                 }
@@ -70,7 +86,7 @@ class LoginActivity : BaseActivity() {
                     } else {
                         AlertDialog.Builder(this)
                             .setTitle("Erreur")
-                            .setMessage("L'utilisateur n'existe pas en local.")
+                            .setMessage("Mot de passe ou email incorrect.")
                             .setPositiveButton("OK") { dialog, _ ->
                                 dialog.dismiss()
                             }
--- app/src/main/java/com/example/only_friends/view/SignUpActivity.kt
+++ app/src/main/java/com/example/only_friends/view/SignUpActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.only_friends.MainActivity
@@ -46,6 +48,13 @@ class SignUpActivity : BaseActivity() {
             val email = findViewById<EditText>(R.id.email).text.toString()
             val password = findViewById<EditText>(R.id.password).text.toString()
             val passwordHash = findViewById<EditText>(R.id.password).text.toString().sha256()
+            val errorMessageTextView = findViewById<TextView>(R.id.error_message)
+
+
+            if (email.isEmpty() || password.isEmpty()) {
+                errorMessageTextView.text = "Veuillez remplir tous les champs."
+                return@setOnClickListener
+            }
             viewModel.signUp(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val firebaseUser = repository.getCurrentUser()
@@ -58,7 +67,16 @@ class SignUpActivity : BaseActivity() {
                     startActivity(intent)
 
                 } else {
-                    // pour les messages d'erreurs et la mannière de les afficher
+                    //recuperer le message d'erreur
+                    val message = task.exception?.message
+                    AlertDialog.Builder(this)
+                        .setTitle("Erreur")
+                        .setMessage(message)
+                        .setPositiveButton("OK") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .show()
+
 
                 }
             }
