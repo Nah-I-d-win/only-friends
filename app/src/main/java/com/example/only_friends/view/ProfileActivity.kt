71a46630cafaa9c4bab5676d087a5e7150031fa9
vithushan <vithushanarul08@gmail.com>
2024-02-27 13:53:48 +0100

    front profil

---
+++ app/src/main/java/com/example/only_friends/view/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.example.only_friends.view
+
+import android.os.Bundle
+import com.example.only_friends.R
+
+
+
+class ProfileActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+
+}
